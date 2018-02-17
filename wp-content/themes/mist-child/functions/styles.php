95dfbd94c4b36d7152cedaed8000793b0f8b714d
tim0thius <timblack.design@gmail.com>
2018-02-17 09:43:02 -0600

    Css tweaks

--- wp-content/themes/mist-child/functions/styles.php
+++ wp-content/themes/mist-child/functions/styles.php
@@ -4,7 +4,7 @@
  * Enqueues parent theme stylesheet
  * ========================================= */
 
-// function zozo_enqueue_child_theme_styles() {
-// 	wp_enqueue_style( 'zozo-child-theme-style', get_stylesheet_uri(), array(), null );
-// }
-// add_action( 'wp_enqueue_scripts', 'zozo_enqueue_child_theme_styles' );
+function zozo_enqueue_child_theme_styles() {
+	// wp_enqueue_style( 'zozo-child-theme-style', get_stylesheet_uri(), array(), null );
+}
+add_action( 'wp_enqueue_scripts', 'zozo_enqueue_child_theme_styles' );
