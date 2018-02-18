6a3af4495718aab3326841e1cf16550566f7a9cf
tim0thius <timblack.design@gmail.com>
2018-02-18 09:33:41 -0600

    Sets up pop up contact form

--- wp-content/themes/mist-child/templates/feature_home.php
+++ wp-content/themes/mist-child/templates/feature_home.php
@@ -2,8 +2,8 @@
 		<div class="block-feature__bg-image"><?php tbd_get_svg('splash_1'); ?></div>
 	<div class="container">
 		<h2 class="block-feature__headline headline">Websites, Logos, and Brand Identity Designed to Grow Your Business.</h2>
-		<!-- <a href="#section-contact" data-target="#section-contact" class="block-feature__button smoothscroll" >set up a free consultation</a> -->
-		<div id="block-feature__menu"><?php zozo_header_content_area( 'main-navigation' ); ?></div>
+		<a class="block-feature__button action--show-contact-form" >set up a free consultation</a>
+		<!-- <div id="block-feature__menu"><?php zozo_header_content_area( 'main-navigation' ); ?></div> -->
 		<div class="block-feature__image-container">
 			<div class="block-feature__image">
 				<?php tbd_get_svg('devices_clean'); ?>
